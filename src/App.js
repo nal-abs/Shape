47b390ccd36a6eda00460220db681183dca24b85
nvn-laufire <nvn@laufire.in>
2022-10-28 12:21:08 +0530

    first approach

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ import { React } from 'react';
 import './App.scss';
 
 const App = () =>
-	<div className="App" role="App">
-		Ready to start.
+	<div role="App">
+		<div className="square">
+			<div className="oneSquare"/>
+			<div className="twoSquare"/>
+			<div className="threeSquare"/>
+		</div>
+		<div className="square">
+			<div className="fourSquare"/>
+			<div className="fiveSquare"/>
+			<div className="sixSquare"/>
+		</div>
+		<div className="square">
+			<div className="sevenSquare"/>
+			<div className="eightSquare"/>
+			<div className="nineSquare"/>
+		</div>
+
 	</div>;
 
 export default App;
