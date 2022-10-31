55f11441b1e8e82bc473339ca5987ca843f3d547
nvn-laufire <nvn@laufire.in>
2022-10-31 23:04:56 +0530

    Circle is introduced.

--- src/component/Circle.js
+++ src/component/Circle.js
@@ -1,15 +1,32 @@
+import { map, range } from '@laufire/utils/collection';
 import { React } from 'react';
+const nine = 9;
+const height = 300;
+const width = 900;
+const divider = 40;
+const oneEighty = 180;
+const radius = 100;
 
-const size = 50;
-const width = `${ size }px`;
-const height = `${ size }px`;
-const left = 'calc(50vw)';
-const top = 'calc(50vh)';
+const circles = map(range(0, nine), (number) => {
+	const radian = number * divider * Math.PI / oneEighty;
+	const y = Math.sin(radian) * radius;
+	const x = Math.cos(radian) * radius;
+
+	return {
+		id: number,
+		style: {
+			width: '50px',
+			height: '50px',
+			top: `${ height + y }px`,
+			left: `${ width + x }px`,
+		},
+	};
+});
+
+const circle = ({ id, style }) =>
+	<div key={ id } style={ style } className="circle"/>;
 
 const Circle = () =>
-	<div
-		style={ { width, height, left, top } }
-		className="circle"
-	/>;
+	<div>{map(circles, circle)}</div>;
 
 export default Circle;
