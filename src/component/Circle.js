3e9d905c0bf0292ff803bba28913c58192f9a5c7
nvn-laufire <nvn@laufire.in>
2022-10-31 19:01:20 +0530

    Circle component done.

--- src/component/Circle.js
+++ src/component/Circle.js
@@ -1,5 +1,15 @@
 import { React } from 'react';
 
-const Circle = () => <div>Circle</div>;
+const size = 50;
+const width = `${ size }px`;
+const height = `${ size }px`;
+const left = 'calc(50vw)';
+const top = 'calc(50vh)';
+
+const Circle = () =>
+	<div
+		style={ { width, height, left, top } }
+		className="circle"
+	/>;
 
 export default Circle;
