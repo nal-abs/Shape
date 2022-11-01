7b375e889292e75137772978eb44d817063b5f1e
nvn-laufire <nvn@laufire.in>
2022-11-01 10:24:29 +0530

    Refactored

--- src/component/Circle.js
+++ src/component/Circle.js
@@ -1,22 +1,24 @@
 import { map, range } from '@laufire/utils/collection';
 import { React } from 'react';
+
 const nine = 9;
 const height = 300;
 const width = 900;
-const divider = 40;
+const size = 50;
 const oneEighty = 180;
 const radius = 100;
+const angle = 360;
 
 const circles = map(range(0, nine), (number) => {
-	const radian = number * divider * Math.PI / oneEighty;
+	const radian = number * (angle / nine) * Math.PI / oneEighty;
 	const y = Math.sin(radian) * radius;
 	const x = Math.cos(radian) * radius;
 
 	return {
 		id: number,
 		style: {
-			width: '50px',
-			height: '50px',
+			width: `${ size }px`,
+			height: `${ size }px`,
 			top: `${ height + y }px`,
 			left: `${ width + x }px`,
 		},
