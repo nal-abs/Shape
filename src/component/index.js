d88401142510fbffe8be8aa6a881603d8489243a
nal-abs <nal@abstractit.in>
2023-09-25 11:26:11 +0530

    Refactored.

--- src/component/index.js
+++ src/component/index.js
@@ -2,22 +2,24 @@ import { map } from '@laufire/utils/collection';
 import { React } from 'react';
 
 const Inputs = (context) => {
-	const { config: { shapes }, state: { selectedShape, space }} = context;
+	const { config: { shapes }, state: { selectedShape, space },
+		actions } = context;
 
-	return <div>
-		<label>Choose a shape</label>
-		<input
-			value={ space }
-			onChange={ (evt) => context.actions.setSpace(evt.target.value) }
-		/>
-		<select
-			name="Shapes"
-			value={ selectedShape }
-			onChange={ (evt) => context.actions.setShape(evt.target.value) }
-		>
-			{map(shapes, (shape) => <option key={ shape }value={ shape }>
-				{shape}</option>)}
-		</select></div>;
+	return (
+		<div>
+			<label>Choose a shape</label>
+			<input
+				value={ space }
+				onChange={ (evt) => actions.setSpace(evt.target.value) }
+			/>
+			<select
+				name="Shapes"
+				value={ selectedShape }
+				onChange={ (evt) => actions.setShape(evt.target.value) }
+			>
+				{map(shapes, (shape) => <option key={ shape } value={ shape }>
+					{shape}</option>)}
+			</select></div>);
 };
 
 export default Inputs;
